72450f73c944e23cbd9782603d06b9bde0b4c995
Safiy Cham <csafiy@yahoo.com>
2019-11-15 20:28:22 -0800

    Updates

---
+++ precache-manifest.1a092765e1b8d0bfec41ae33e4881247.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066c8a9d1cfe58458ad61264c6d7ee78",
+    "url": "/drum-it/index.html"
+  },
+  {
+    "revision": "8d16db4692b483216da4",
+    "url": "/drum-it/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5b83fec631144bcfa6b8",
+    "url": "/drum-it/static/js/2.3aecc047.chunk.js"
+  },
+  {
+    "revision": "8d16db4692b483216da4",
+    "url": "/drum-it/static/js/main.c8a7d29d.chunk.js"
+  },
+  {
+    "revision": "9a049a47c43a2a68e82a",
+    "url": "/drum-it/static/js/runtime-main.a3fd06cb.js"
+  },
+  {
+    "revision": "a4a84a111221cedd9f13e942acd89be1",
+    "url": "/drum-it/static/media/crowd1.a4a84a11.jpg"
+  },
+  {
+    "revision": "fccf6d94bc66fca7cdf518cc143abf5d",
+    "url": "/drum-it/static/media/drum-set.fccf6d94.png"
+  },
+  {
+    "revision": "697d4a53cc2312043cf531dc3d20394a",
+    "url": "/drum-it/static/media/stage.697d4a53.jpg"
+  }
+]);
